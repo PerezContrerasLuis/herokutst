fb3817cb84fdd000a2c9764aa052b1a8284390dc
PerezContrerasLuis <luisperezcontreras@gmail.com>
2021-03-26 13:58:45 -0600

    new quagga

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Ler Barcode </title>
 </head>
+
 <body>
-    <h1> Codigos de Barras </h1>
+    <div id="resultado"></div>
+    <div id="camera"></div>
+
+    <script src="quagga.min.js"></script>
+
+    <script>
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                target: document.querySelector('#camera')    
+            },
+            
+            decoder: {
+                readers: ["code_128_reader","ean_reader"]
+            }
+        }, function (err) {
+            if (err) {
+                console.log(err);
+                return
+            }
+            console.log("Initialization finished. Ready to start");
+            Quagga.start();
+        });
+
+        Quagga.onDetected(function (data) {
+            console.log(data.codeResult.code);
+            document.querySelector('#resultado').innerText = data.codeResult.code;
+        });
+
+    </script>
+
 </body>
+
 </html>
