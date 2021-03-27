6642c94315e7668eb72d39a910c697fe14766a1c
PerezContrerasLuis <luisperezcontreras@gmail.com>
2021-03-26 21:27:47 -0600

    quagga 1

--- Copia de index.php
+++ Copia de index.php
@@ -9,7 +9,6 @@
 </head>
 
 <body>
-    <h1> test de quagga js </h1>
     <div id="resultado"></div>
     <div id="camera"></div>
 
@@ -23,7 +22,6 @@
                 target: document.querySelector('#camera')    
             },
             frequency: 10,
-            locate: true,
             decoder: {
                 readers: ["code_128_reader","ean_reader"]
             }
@@ -35,30 +33,6 @@
             console.log("Initialization finished. Ready to start");
             Quagga.start();
         });
-
-        Quagga.onProcessed(function(result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
-
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-                });
-            }
-
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-            }
-
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-            }
-        }
-    });
 
         Quagga.onDetected(function (data) {
             console.log(data.codeResult.code);
